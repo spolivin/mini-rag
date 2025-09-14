5a2a92fead406878c077f922b80ece4aff4351bb
Sergey Polivin <s.polivin@gmail.com>
2025-09-14 19:38:48 +0200

    refactor(scripts): modify run script

--- run.py
+++ run.py
@@ -1,70 +1,56 @@
 import argparse
 
+import yaml
+
 from mini_rag.pipeline import RAGPipeline
 
-parser = argparse.ArgumentParser(description="Process some documents.")
-parser.add_argument(
-    "--source-doc", required=True, type=str, help="Path to the document"
-)
+parser = argparse.ArgumentParser(description="RAG script")
+parser.add_argument("--source-doc", type=str, help="Path to the document")
 parser.add_argument(
     "--query",
-    required=True,
     type=str,
     help="Query string to search in the index",
-)
-parser.add_argument("--chunk-size", type=int, help="Size of each chunk", default=300)
-parser.add_argument("--overlap", type=int, help="Overlap between chunks", default=50)
-parser.add_argument(
-    "--gen-model",
-    type=str,
-    help="Text generation model name",
-    default="google/flan-t5-small",
-)
-parser.add_argument(
-    "--embedding-model",
-    type=str,
-    help="SentenceTransformer model name",
-    default="sentence-transformers/all-mpnet-base-v2",
-)
-parser.add_argument(
-    "--cross-encoder-model",
-    type=str,
-    help="Cross-Encoder model name",
-    default="cross-encoder/ms-marco-MiniLM-L-6-v2",
 )
 parser.add_argument(
     "--top-k", type=int, help="Number of top results to retrieve", default=5
 )
-parser.add_argument("--verbose", action="store_true", help="Increase output verbosity")
+parser.add_argument(
+    "--config-file", type=str, help="Path to configuration file", default=None
+)
 args = parser.parse_args()
 
 
 def main():
-    # Initialize the retrieval pipeline
-    pipeline = RAGPipeline(
-        source_doc_path=args.source_doc,
-        gen_model_name=args.gen_model,
-        embedding_model_name=args.embedding_model,
-        cross_encoder_model_name=args.cross_encoder_model,
-    )
-    # Run the pipeline with the provided query
-    answer, candidates, scores = pipeline(
-        query=args.query,
-        top_k=args.top_k,
-        chunk_size=args.chunk_size,
-        overlap=args.overlap,
-        verbose=args.verbose,
-    )
+    if args.config_file:
+        with open(args.config_file) as f:
+            config = yaml.safe_load(f)
+        source_doc_path = config["source_doc"]
+        query = config["query"]
+        top_k = config["top_k"]
+    else:
+        source_doc_path = args.source_doc
+        query = args.query
+        top_k = args.top_k
 
-    # Print the retrieved candidates
-    for i, candidate in enumerate(candidates):
-        print(100 * "-")
-        print(f"[RETRIEVAL RESULT #{i + 1}, Score: {scores[i]:.4f}]: '{candidate}'")
-    print(100 * "-")
+    # Running RAG
+    rag_pipeline = RAGPipeline(source_doc_path=source_doc_path)
 
-    # Print the final answer from the LLM
-    print(f"\nQuestion: {args.query}\n")
-    print(f"Answer: {answer}\n")
+    # Retieving LLM response, ranked chunks of text and relevance scores for ranked chunks
+    answer, ranked_candidates, ranked_scores = rag_pipeline(query=query, top_k=top_k)
+
+    print("\n" + "=" * 60)
+    print("🔎 Retrieved/Ranked Chunks".center(60))
+    print("=" * 60)
+    for i, (candidate, score) in enumerate(zip(ranked_candidates, ranked_scores), 1):
+        print(f"\nRetrieval result #{i} | Relevance score: {score:.4f}")
+        print("-" * 60)
+        print(candidate.strip())
+    print("=" * 60)
+
+    print("\n❓ Question:")
+    print(query + "\n")
+    print("💡 Answer:")
+    print(f"{answer}\n")
 
 
 if __name__ == "__main__":
